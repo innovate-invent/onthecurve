3f1a09b2374181d2a74fb260739418bdda1e4855
Nolan Woods <innovate.invent@gmail.com>
2020-04-11 19:30:04 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-app id="app">
+        <v-app-bar absolute app dense>
+            <Navigation :routes="routes" />
+        </v-app-bar>
+
+        <v-content>
+            <v-container fluid>
+                <router-view :class="($route.meta && $route.meta.slug) || ''" />
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Navigation from '@/components/Navigation.vue';
+
+import routes from '@/router/main';
+
+@Component({
+    components: { Navigation }
+})
+export default class App extends Vue {
+    private routes = routes;
+}
+</script>
+
+<style scoped></style>
+
+<style lang="scss">
+@import '@/assets/main.scss';
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+import main from './main';
+
+const router = new VueRouter({
+    routes: main,
+    mode: 'history'
+});
+
+export default router;
